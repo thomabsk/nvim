630c832eb76636fe96786ee0251fc9953e8b5ac7
thomabsk <thomabsk@stud.ntnu.no>
2023-04-22 13:41:32 +0200

    initial commit

---
+++ lua/borge/core/colorscheme.lua
@@ -0,0 +1,7 @@
+local status, _ = pcall(vim.cmd, "colorscheme kanagawa")
+if not status then
+    print("Colorscheme not found!")
+    return
+end
+
+
---
+++ lua/borge/plugins/lsp/null-ls.lua
@@ -0,0 +1,23 @@
+-- import null-ls plugin safely
+local setup, null_ls = pcall(require, "null-ls")
+if not setup then
+  return
+end
+
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+
+-- to setup format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+null_ls.setup({
+    sources = {
+        formatting.prettier,
+        formatting.stylua,
+        diagnostics.eslint_d,
+        formatting.black,
+        diagnostics.ruff,
+    }
+
+})
